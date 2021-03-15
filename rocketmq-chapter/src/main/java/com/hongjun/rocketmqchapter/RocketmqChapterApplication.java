b3feb3173a8c6767b73c7f216f21d4acb0a71779
hongjun500 <bhj.hong@qq.com>
2021-03-16 00:23:58 +0800

    Initial commit

---
+++ rocketmq-chapter/src/main/java/com/hongjun/rocketmqchapter/RocketmqChapterApplication.java
@@ -0,0 +1,13 @@
+package com.hongjun.rocketmqchapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqChapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqChapterApplication.class, args);
+    }
+
+}
---
+++ rocketmq-chapter/src/test/java/com/hongjun/rocketmqchapter/RocketmqChapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongjun.rocketmqchapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqChapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
